6518864b18621a1cf3c968f755eaba610cc72779
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2022-11-14 15:18:12 +0100

    added new PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || `https://backend-portfolio-eight.vercel.app/`;
 require("dotenv").config();
 const methodOverride = require("method-override");
 const cors = require("cors");
